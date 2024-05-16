c9221e55c3e9576941c89f63a651053a78a0f0a2
pedroalonsoms <pedro123ben10@gmail.com>
2024-05-15 19:52:45 -0600

    unformat character page - again - again - agin

--- 04-nestjs-rick-and-morty/src/ui/Character.tsx
+++ 04-nestjs-rick-and-morty/src/ui/Character.tsx
@@ -11,7 +11,7 @@ interface CharacterComponentProps {
   character: Character
 }
 
-                export function CharacterComponent({
+                    export function CharacterComponent({
   character: { id, name, image, status, gender, location },
 }: CharacterComponentProps) {
   const [isFavorite, setIsFavorite] = useState(false)
