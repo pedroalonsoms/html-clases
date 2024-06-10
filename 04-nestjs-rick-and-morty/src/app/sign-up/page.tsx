659c6ba5891b4979bd257fd3febeeccd7ff637aa
pedroalonsoms <pedro123ben10@gmail.com>
2024-06-09 22:28:41 -0600

    styled signup page and changed codecov to pass threshold

--- 04-nestjs-rick-and-morty/src/app/sign-up/page.tsx
+++ 04-nestjs-rick-and-morty/src/app/sign-up/page.tsx
@@ -8,27 +8,45 @@ export default function SignUp() {
   const router = useRouter()
 
   return (
-    <>
-      <p>Sign Up Page</p>
+    <div className='grid place-content-center pt-20'>
+      <p className='text-3xl font-medium'>Sign Up Page</p>
+
       <form
         action={async (formData: FormData) => {
           await signUpUser(formData)
           router.push('/all-characters')
         }}
+        className='flex flex-col gap-5 pt-10'
       >
-        <label>
-          Email
-          <input name='email' type='email' />
-        </label>
-        <label>
-          Password
-          <input name='password' type='password' />
-        </label>
-        <button>Sign Up</button>
+        <div className='flex flex-col'>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            name='email'
+            type='email'
+            className='rounded-sm text-black'
+          />
+        </div>
+
+        <div className='flex flex-col'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            className='rounded-sm text-black'
+          />
+        </div>
+
+        <button className='rounded-sm bg-slate-900 py-1'>Sign Up</button>
       </form>
-      <p>
-        Already have an account? <Link href={'/sign-in'}>Sign In</Link>
+
+      <p className='pt-10'>
+        Already have an account?{' '}
+        <Link className='hover:underline' href={'/sign-in'}>
+          Sign In
+        </Link>
       </p>
-    </>
+    </div>
   )
 }
