60c04928c2fa12d2140f3c1641ec7390cdf1a2c0
pedroalonsoms <pedro123ben10@gmail.com>
2024-06-09 22:54:57 -0600

    finish styling up login and register pages

--- 04-nestjs-rick-and-morty/src/app/sign-in/page.tsx
+++ 04-nestjs-rick-and-morty/src/app/sign-in/page.tsx
@@ -3,50 +3,70 @@
 import { signInUser } from '@/services/signInUser'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function SignIn() {
+  const [isError, setIsError] = useState<boolean>(false)
   const router = useRouter()
 
   return (
-    <div className='grid place-content-center pt-20'>
-      <p className='text-3xl font-medium'>Sign In Page</p>
+    <>
+      {isError && (
+        <p className='bg-slate-900 py-2 text-center'>
+          There was an error signing in, please check your credentials
+        </p>
+      )}
 
-      <form
-        action={async (formData: FormData) => {
-          await signInUser(formData)
-          router.push('/all-characters')
-        }}
-        className='flex flex-col gap-5 pt-10'
-      >
-        <div className='flex flex-col'>
-          <label htmlFor='email'>Email</label>
-          <input
-            id='email'
-            name='email'
-            type='email'
-            className='rounded-sm text-black'
-          />
-        </div>
+      <div className='grid place-content-center pt-20'>
+        <p className='text-3xl font-medium'>Sign In Page</p>
 
-        <div className='flex flex-col'>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            name='password'
-            type='password'
-            className='rounded-sm text-black'
-          />
-        </div>
+        <form
+          action={async (formData: FormData) => {
+            try {
+              await signInUser(formData)
+              router.push('/all-characters')
+            } catch (error) {
+              setIsError(true)
+            }
+          }}
+          className='flex flex-col gap-5 pt-10'
+        >
+          <div className='flex flex-col'>
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              name='email'
+              type='email'
+              className='rounded-sm text-black'
+              onChange={() => {
+                setIsError(false)
+              }}
+            />
+          </div>
 
-        <button className='rounded-sm bg-slate-900 py-1'>Sign In</button>
-      </form>
+          <div className='flex flex-col'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              className='rounded-sm text-black'
+              onChange={() => {
+                setIsError(false)
+              }}
+            />
+          </div>
 
-      <p className='pt-10'>
-        Not registered?{' '}
-        <Link className='hover:underline' href={'/sign-up'}>
-          Sign Up
-        </Link>
-      </p>
-    </div>
+          <button className='rounded-sm bg-slate-900 py-1'>Sign In</button>
+        </form>
+
+        <p className='pt-10'>
+          Not registered?{' '}
+          <Link className='hover:underline' href={'/sign-up'}>
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </>
   )
 }
--- 04-nestjs-rick-and-morty/src/app/sign-up/page.tsx
+++ 04-nestjs-rick-and-morty/src/app/sign-up/page.tsx
@@ -3,50 +3,70 @@
 import { signUpUser } from '@/services/signUpUser'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function SignUp() {
+  const [isError, setIsError] = useState<boolean>(false)
   const router = useRouter()
 
   return (
-    <div className='grid place-content-center pt-20'>
-      <p className='text-3xl font-medium'>Sign Up Page</p>
+    <>
+      {isError && (
+        <p className='bg-slate-900 py-2 text-center'>
+          There was an error signing up, please check your credentials
+        </p>
+      )}
 
-      <form
-        action={async (formData: FormData) => {
-          await signUpUser(formData)
-          router.push('/all-characters')
-        }}
-        className='flex flex-col gap-5 pt-10'
-      >
-        <div className='flex flex-col'>
-          <label htmlFor='email'>Email</label>
-          <input
-            id='email'
-            name='email'
-            type='email'
-            className='rounded-sm text-black'
-          />
-        </div>
+      <div className='grid place-content-center pt-20'>
+        <p className='text-3xl font-medium'>Sign Up Page</p>
 
-        <div className='flex flex-col'>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            name='password'
-            type='password'
-            className='rounded-sm text-black'
-          />
-        </div>
+        <form
+          action={async (formData: FormData) => {
+            try {
+              await signUpUser(formData)
+              router.push('/all-characters')
+            } catch (error) {
+              setIsError(true)
+            }
+          }}
+          className='flex flex-col gap-5 pt-10'
+        >
+          <div className='flex flex-col'>
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              name='email'
+              type='email'
+              className='rounded-sm text-black'
+              onChange={() => {
+                setIsError(false)
+              }}
+            />
+          </div>
 
-        <button className='rounded-sm bg-slate-900 py-1'>Sign Up</button>
-      </form>
+          <div className='flex flex-col'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              className='rounded-sm text-black'
+              onChange={() => {
+                setIsError(false)
+              }}
+            />
+          </div>
 
-      <p className='pt-10'>
-        Already have an account?{' '}
-        <Link className='hover:underline' href={'/sign-in'}>
-          Sign In
-        </Link>
-      </p>
-    </div>
+          <button className='rounded-sm bg-slate-900 py-1'>Sign Up</button>
+        </form>
+
+        <p className='pt-10'>
+          Already have an account?{' '}
+          <Link className='hover:underline' href={'/sign-in'}>
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </>
   )
 }
