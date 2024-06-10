6a1261fb3a10da92c24bdfce4436a12c1857cf0b
pedroalonsoms <pedro123ben10@gmail.com>
2024-06-10 00:34:27 -0600

    togglefavchar now calls the db directly

--- 04-nestjs-rick-and-morty/src/services/toggleFavoriteCharacter.ts
+++ 04-nestjs-rick-and-morty/src/services/toggleFavoriteCharacter.ts
@@ -4,14 +4,20 @@ import { auth } from '@/auth'
 import { db } from '@/db'
 import { favorites } from '@/db/schema'
 import { eq, and } from 'drizzle-orm'
-import { isFavoriteCharacter } from './isFavoriteCharacter'
 
 export async function toggleFavoriteCharacter(characterId: number) {
   const session = await auth()
   const userId = session?.user?.id
   if (!userId) throw new Error('User must be authenticated')
 
-  const isFavorite = await isFavoriteCharacter(characterId)
+  const res = await db
+    .select()
+    .from(favorites)
+    .where(
+      and(eq(favorites.characterId, characterId), eq(favorites.userId, userId)),
+    )
+
+  const isFavorite = res.length === 1
 
   if (isFavorite) {
     await db
